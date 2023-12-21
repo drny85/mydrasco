852609e2d285cfea8db6a472c8d8629678f9fce6
Robert Melendez <melendez@robertdev.net>
2023-12-21 13:41:13 -0500

    change BYOD

--- constant/index.ts
+++ constant/index.ts
@@ -1,4 +1,4 @@
 export const BONUS_EXPIRATION_DATE = '12/31/2023';
 export const ULTIMATE_BYOD_VALUE = 540 / 36;
 export const PLUS_BYOD_VALUE = 360 / 36;
-export const WELCOME_BYOD_VALUE = 180 / 36;
+export const WELCOME_BYOD_VALUE = 360 / 36;
